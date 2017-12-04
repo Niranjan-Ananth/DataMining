601d49c360859adae056f4d815c31e0c3a54509e
Niranjan-Ananth <niran.96@gmail.com>
2017-12-04 13:05:26 +0530

    Added Comments

--- KNNClassifier/src/Demo.java
+++ KNNClassifier/src/Demo.java
@@ -1,4 +1,3 @@
-import java.awt.Window.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -11,6 +10,7 @@ public class Demo {
 		int numberOfPoints = 10;
 		int k=5;
 		//Point kNearestNeighbours[] = new Point[10];
+		//Generate random points as the data set, and add it to the list.
 		System.out.println("The points generated are: ");
 		ArrayList<Point> kNearestNeighbours = new ArrayList<>();
 		for(int i=0; i<numberOfPoints; i++) {
@@ -21,8 +21,11 @@ public class Demo {
 			points.add(p);
 			System.out.println(x + " " + y + " " + type);
 		}
+		//Generate a random point to be classified.
 		Point pointToBeClassified = new Point(r.nextInt(100), r.nextInt(100), -1, 0);
 		System.out.println("The point to be classified is: " + pointToBeClassified.x + " " + pointToBeClassified.y);
+		
+		//Calculate the distance between the points and the point to be classified and store it.
 		for(int i=0; i<numberOfPoints; i++) {
 			points.get(i).distance = pointToBeClassified.calculateDistance(points.get(i));
 		}
@@ -32,6 +35,8 @@ public class Demo {
 			System.out.println(p.x + " " + p.y + " " + p.type + " " + p.distance);
 		}
 		System.out.println("---------------------");*/
+		
+		//Sort the points list, based on the distance between the points and the point to be classified, in ascending order. 
 		Collections.sort(points, new PointsComaparator());
 
 		/*for(int i=0; i<numberOfPoints; i++) {
@@ -40,6 +45,7 @@ public class Demo {
 		}
 		System.out.println("---------------------");*/
 		
+		//Find the dominating class, amongst the k nearest neighbours.
 		System.out.println("The k-Nearest Neighbours are: ");
 		int score = 0;
 		for(int i=0; i<k; i++) {
@@ -51,7 +57,7 @@ public class Demo {
 				score++;
 		}
 		
-		
+		//Classify the point given based on the knn.
 		if(score > 0) {
 			pointToBeClassified.type = 1;
 			System.out.println("Type of the Point to be classified: Type 1");
