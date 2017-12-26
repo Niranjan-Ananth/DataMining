61f7b093f7448af508d5bf8312338817d1aefd40
Niranjan-Ananth <niran.96@gmail.com>
2017-12-26 16:07:05 +0530

    Made changes to the perceptron program

--- Perceptron/src/Demo.java
+++ Perceptron/src/Demo.java
@@ -4,85 +4,92 @@ public class Demo {
 
 	public static void main(String[] args) {
 		int numberOfRecords = 5;
-		int trainData[][] = new int[numberOfRecords][5];
-		double weights[] = new double[3];
-		double bias = 0.4, learningRate = 0.5;
+		int numberOfInputFeatures = 3;
+		int[][] trainingData = new int[numberOfRecords][numberOfInputFeatures+1]; //One extra after the numberofInputFeatures, for the output
+		double weights[] = new double[numberOfInputFeatures];
+		double learningRate = 0.5, bias = 0.4;
 		Random r = new Random();
-		//Generating training data
+		
+		//Generate random training data
+		int score;
 		for(int i=0; i<numberOfRecords; i++) {
-			//Random inputs
-			int x1 = r.nextInt(2);
-			int x2 = r.nextInt(2);
-			int x3 = r.nextInt(2);
-			int score = 0, y;
-			
-			//Using score to find the number of 1's in each record of input data.
-			if(x1 > 0) 
-				score++;
-			if(x2 > 0)
-				score++;
-			if(x3 > 0)
-				score++;
-			
-			//Output is 1, if there are at least two 1's. Else, it's -1.
-			if(score>1) {
-				y = 1;
+			score = 0;
+			for(int j=0; j<numberOfInputFeatures; j++) {
+				int x = r.nextInt(2);
+				trainingData[i][j] = x;
+				if(x==0) {
+					score--;
+				}
+				else {
+					score++;
+				}
 			}
+			//If more 1's than 0's, output is 1
+			if(score > 0) {
+				trainingData[i][numberOfInputFeatures] = 1;
+			}
+			//Else output is 0
 			else {
-				y = -1;
+				trainingData[i][numberOfInputFeatures] = -1;
 			}
-			
-			trainData[i][0] = x1;
-			trainData[i][1] = x2;
-			trainData[i][2] = x3;
-			trainData[i][3] = y;
 		}
 		
+		//Show the training data
 		for(int i=0; i<numberOfRecords; i++) {
-			System.out.println(trainData[i][0] + " " + trainData[i][1] + " " + trainData[i][2] + " " + trainData[i][3]);
+			for(int j=0; j<numberOfInputFeatures+1; j++) {
+				System.out.print(trainingData[i][j] + " ");
+			}
+			System.out.println();
 		}
 		
-		//Initialize weights to a random double value between 0 and 1.
-		weights[0] = r.nextDouble();
-		weights[1] = r.nextDouble();
-		weights[2] = r.nextDouble();
+		//Random initial weights
+		for(int i=0; i<numberOfInputFeatures; i++) {
+			weights[i] = r.nextDouble();
+		}
 		
+		//Array to hold the predicted outputs
 		int pred[] = new int[numberOfRecords];
 		
-		double totalError = 998;
-		double totalPrevError = 999;
-		while(totalError < totalPrevError) {
+		double totalPrevError = 999, totalError = 998;
+		while((totalError < totalPrevError) && (totalError!=0)) {
 			totalPrevError = totalError;
 			totalError = 0;
 			for(int i=0; i<numberOfRecords; i++) {
-				double a = (weights[0]*trainData[i][0] + weights[1]*trainData[i][1] + weights[2]*trainData[i][2] - bias);
-				int y1;
-				if(a > 0)
-					y1 = 1;
-				else
-					y1 = -1;
-				int y = trainData[i][3];
-				pred[i] = y1;
-				totalError += Math.abs(y-y1);
-				for(int j=0; j<3; j++) {
-					weights[j] = weights[j] + learningRate*(y - y1)*trainData[i][j];
-					//System.out.print(weights[j] + " ");
+				double a = 0;
+				//Find the output of the perceptron
+				for(int j=0; j<numberOfInputFeatures; j++) {
+					a += weights[j]*trainingData[i][j];
 				}
-				//System.out.println();
+				a -= bias;
+				int y1;
+				if(a > 0) {
+					y1 = 1;
+				}
+				else {
+					y1 = -1;
+				}
+				pred[i] = y1;
+				int y = trainingData[i][numberOfInputFeatures];
+				totalError += Math.abs(y-y1);
+				
+				//Update weights after each input
+				for(int j=0; j<numberOfInputFeatures; j++) {
+					weights[j] = weights[j] + learningRate*(y-y1)*trainingData[i][j];
+				}
+				
 			}
+			
 			System.out.print("Predicted values: ");
 			for(int i=0; i<numberOfRecords; i++) {
 				System.out.print(pred[i] + " ");
 			}
+			
 			System.out.println("Error: " + totalError);
-			/*-System.out.print("Weights: ");
-			for(int i=0; i<3; i++) {
-				System.out.print(weights[i] + " ");
-			}
-			System.out.println();*/
+			
 		}
 		
-		System.out.println("Final error: " + totalError);
+		System.out.println("Final Error: " + totalError);
 		
 	}
+
 }
